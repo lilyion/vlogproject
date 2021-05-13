16508ebc35951ac3b41b3f5606278d66ca476ec5
lilyion <likelilyion@gmail.com>
2021-05-13 22:12:28 +0900

    22

--- Vlog/settings.py
+++ Vlog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'PORT'  : '5432',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)\
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
